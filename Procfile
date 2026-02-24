8faabc1c9d6520077466e3f4be5fd14f3412a33a
Fabio Hams Valois <92942417+Neon18H@users.noreply.github.com>
2026-02-24 17:15:32 -0500

    Fix static serving setup and ship enterprise inventory UI refresh

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py migrate && python manage.py collectstatic --noinput
-web: gunicorn gestion_ropa.wsgi:application
+release: DJANGO_SETTINGS_MODULE=gestion_ropa.settings python manage.py migrate && DJANGO_SETTINGS_MODULE=gestion_ropa.settings python manage.py collectstatic --noinput
+web: DJANGO_SETTINGS_MODULE=gestion_ropa.settings gunicorn gestion_ropa.wsgi:application
--- gestion_ropa/settings.py
+++ gestion_ropa/settings.py
@@ -68,10 +68,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
