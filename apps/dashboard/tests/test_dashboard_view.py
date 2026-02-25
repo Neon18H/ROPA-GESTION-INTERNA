d3bc97cbe5d2d7898a4196ffed944e859fbf26cb
Fabio Hams Valois <92942417+Neon18H@users.noreply.github.com>
2026-02-24 19:43:20 -0500

    Fix dashboard template extends order and add root render test

--- apps/dashboard/tests/test_dashboard_view.py
+++ apps/dashboard/tests/test_dashboard_view.py
@@ -5,6 +5,15 @@ from apps.accounts.models import Organization, User
 
 
 class DashboardViewTests(TestCase):
+    def test_root_url_renders_for_logged_in_user(self):
+        org = Organization.objects.create(name='Org')
+        user = User.objects.create_user(username='root', password='pass1234', organization=org, role=User.Role.ADMIN)
+
+        self.assertTrue(self.client.login(username='root', password='pass1234'))
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+
     def test_dashboard_renders_without_sales_data(self):
         org = Organization.objects.create(name='Org')
         user = User.objects.create_user(username='dash', password='pass1234', organization=org, role=User.Role.ADMIN)
